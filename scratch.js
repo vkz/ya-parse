16e98f5e2881810d06b7ac321976e9ddf8be085e
vkz <vladilen.kozin@gmail.com>
2014-05-20 16:06:37 +0400

    First attempt at solving Problem 2-3. Doesn't track scopes properly.

--- scratch.js
+++ scratch.js
@@ -31,6 +31,7 @@ exports.toLiteral = toLiteral;
 
 // ---------------------- Problem 2-2: obj[key] ------------------------ //
 function identifierLike(str) {
+    // cheat to avoid using RegExps
     var toks = esprima.tokenize(str);
     return toks.length === 1 && toks[0].type === "Identifier";
 }
@@ -55,8 +56,67 @@ exports.toDot = toDot;
 
 // ---------------------- Problem 2-3: scope --------------------------- //
 
+function createsScope(node) {
+    var type = node.type;
+    return type === 'Program' || type === 'FunctionDeclaration' || type === 'FunctionExpression';
+}
+
 function replaceThis(src) {
 
+    // Enter: when 'Program' or 'FunctionDeclaration' or 'FunctionExpression'
+    // - push new scope,
+    // - set currentScope to the pushed one
+    // - for every 'ThisExpression' push it onto current scope
+
+    // Leave: when 'Program' or 'FunctionDeclaration' or 'FunctionExpression'
+    // - pop scope
+    // - if scope.length > 2
+    // - - scope.forEach node
+    // - - - replace node.object with { name : "_t", type : "Identifier"}
+    // - prepend function's body with 'var _t = this;'
+
+    var ast = esprima.parse(src),
+        scopes = [],
+        newScope = function genEmptyScope() {var empty = []; return empty;};
+
+    estraverse.traverse(ast, {
+        enter: function(node, parent) {
+            if (createsScope(node)) {
+                // push new scope onto scopes
+                scopes.push([]);
+
+            } else if (node.type === 'ThisExpression') {
+                var currentScope = scopes[scopes.length - 1];
+                currentScope.push(node);
+
+            }
+        },
+
+        leave: function(node, parent) {
+
+            var currentScope,
+                bodyPrefix;
+
+            if (createsScope(node)) {
+                currentScope = scopes.pop();
+                if (currentScope.length > 2) {
+
+                    // replace every `this` in scope with `_t`
+                    currentScope.forEach (function (node) {
+                        node.type = "Identifier";
+                        node.name = "_t";
+                    });
+
+                    // add 'var _t = this;' declaration
+                    bodyPrefix = esprima.parse('var _t = this;').body;
+                    node.body = bodyPrefix.concat(node.body);
+
+                }
+            }
+
+        }
+    });
+    return escodegen.generate(ast);
 }
 
 exports.replaceThis = replaceThis;
